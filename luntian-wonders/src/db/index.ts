c7de946db9cb844260a778932853a87e458f72e3
blackbangkeros <davzred008@gmail.com>
2026-01-10 12:46:51 +0800

    Create index.ts

---
+++ luntian-wonders/src/db/index.ts
@@ -0,0 +1,13 @@
+import { neon } from '@neondatabase/serverless';
+import { drizzle } from 'drizzle-orm/neon-http';
+import * as dotenv from 'dotenv';
+
+// Load env vars
+dotenv.config({ path: '.env.local' });
+
+if (!process.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL is missing');
+}
+
+const sql = neon(process.env.DATABASE_URL);
+export const db = drizzle(sql);
